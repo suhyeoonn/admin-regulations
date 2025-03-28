a95eaf346dbdbedbce522b5ab36e4be5f9c6d48a
suhyeoonn <suhyeon1740@gmail.com>
2025-03-28 17:54:08 +0900

    디비 에러 발생 시 로그 출력

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,13 @@
 import oracledb from "oracledb";
 
 // Oracle 클라이언트 초기화
-oracledb.initOracleClient();
+try {
+  oracledb.initOracleClient();
+} catch (err) {
+  console.error("Oracle 클라이언트 초기화 실패:", err);
+}
 
-const pool = oracledb.createPool({
+const poolPromise = oracledb.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   connectString: process.env.DB_CONNECT_STRING, // 예: "localhost:1521/XEPDB1"
@@ -12,4 +16,4 @@ const pool = oracledb.createPool({
   poolIncrement: 0,
 });
 
-export default pool;
+export default poolPromise;
